62e0da7ea0ca00719993fae13246cc44e3ce431a
DilbarHussain <dilbar.bskdkf20@iba-suk.edu.pk>
2023-03-18 13:13:35 -0700

    Task#04
    Task 04  Lists Introduction (Changing, Adding, Removing, Organizing, Indexing in Lists), Looping in Python, Looping Over Lists, Avoiding

---
+++ Bytewise_task04.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#changing item lists
+list1 = ["apple", "banana", "cherry"]
+list1[1] = "blackcurrant"
+print(list1)
+
+
+# In[3]:
+
+
+#Adding list items
+list2 = ["apple", "banana", "cherry"]
+list2.append("orange")
+print(list2)
+
+
+# In[4]:
+
+
+#Organizing lists
+prime_numbers = [11, 3, 7, 5, 2]
+prime_numbers.sort()
+
+print(prime_numbers)
+
+
+# In[5]:
+
+
+#Indexing In lists
+animals = ['cat', 'dog', 'rabbit', 'horse']
+index = animals.index('dog')
+
+
+print(index)
+
+
+# In[6]:
+
+
+#Python Loops
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+
+
+# In[7]:
+
+
+#looping Over the lists
+# Python3 code to iterate over a list
+list = [1, 3, 5, 7, 9]
+for i in list:
+    print(i)
+
+
+# In[8]:
+
+
+#Tuples in python
+tuple1 = ("apple", "banana", "cherry")
+print(tuple1)
+
+
+# In[ ]:
+
+
+
+
