86b14e1b341d67b62e57f734e81eadc9e0220583
DilbarHussain <dilbar.bskdkf20@iba-suk.edu.pk>
2023-03-18 03:01:03 -0700

    Task#3
    With code

---
+++ Kicking_Off_with_Python_Bytewise.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Kicking Off with Python (Syntax, Variables, Datatypes, Code Execution)
+#Syntax
+print("Hello I am Dilbar At Bytewise Fellowship")
+
+
+# In[3]:
+
+
+a = 1
+b = 2
+print(a+b)
+
+
+# In[4]:
+
+
+c = a*b
+
+
+# In[5]:
+
+
+print(c)
+
+
+# In[9]:
+
+
+#Variables and DataTypes
+z = int(input("Plz Enter Number"))
+print("You Enter The Number:---> ", z)
+print(type(z))
+
+
+# In[10]:
+
+
+x = "Hello Dilbar"
+
+
+# In[11]:
+
+
+print(x)
+print(type(x))
+
+
+# In[12]:
+
+
+x = 20.5
+print(x)
+print(type(x)) 
+
+
+# In[13]:
+
+
+x = 1j
+print(x)
+print(type(x)) 
+
+
+# In[14]:
+
+
+x = ["apple", "banana", "cherry"]
+print(x)
+print(type(x)) 
+
+
+# In[15]:
+
+
+x = ("apple", "banana", "cherry")
+print(type(x)) 
+
+
+# In[16]:
+
+
+x = {"name" : "Dilbar", "age" : 23}
+print(x)
+print(type(x)) 
+
+
+# In[ ]:
+
+
+
+
