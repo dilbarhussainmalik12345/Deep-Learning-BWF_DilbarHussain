608c6321da037284f0c515a5662a01f9ef7ae084
DilbarHussain <dilbar.bskdkf20@iba-suk.edu.pk>
2023-04-25 10:50:26 -0700

    Task_19

---
+++ LabTask_19.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Learn Deep learning
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+tensor = np.array([8, 16, 32, 64])
+
+
+# In[4]:
+
+
+tensor.shape
+
+
+# In[5]:
+
+
+tensor.ndim
+
+
+# In[6]:
+
+
+tensor = np.array([[2, 10, 20, 22],[8, 16,32,64], [5,10, 15,20]])
+
+
+# In[7]:
+
+
+tensor.shape
+
+
+# In[8]:
+
+
+tensor.ndim
+
+
+# In[ ]:
+
+
+
+
